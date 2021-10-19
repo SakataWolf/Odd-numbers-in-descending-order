83ccafd795a6fd6442ce53669feb2097e360ab79
SakataWolf <87884401+SakataWolf@users.noreply.github.com>
2021-10-19 17:39:59 -0300

    Add files via upload

---
+++ Numeros impares em Ordem decrescente/Numeros impares em Ordem decrescente/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Numeros_impares_em_Ordem_decrescente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Números impares\n\n");
+            Console.WriteLine("Aperte enter pra continuar");
+            Console.ReadKey();
+            int a, b, x, y;
+
+            Console.Write("Digite um valor: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Digite outro valor: ");
+            y = int.Parse(Console.ReadLine());
+
+            if(x >= y)
+            {
+                a = x;
+            }
+            else
+            {
+                a = y;
+            }
+            if (x <= y)
+            {
+                b = x;
+            }
+            else
+            {
+                b = y;
+            }
+            Console.WriteLine("\n");
+            for (int i = a; i > b; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(+i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
